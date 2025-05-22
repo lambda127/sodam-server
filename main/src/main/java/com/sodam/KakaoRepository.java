f64d0bd0ce0389a6123386d5d3db08fd6d271d6e
nanakyeong <dlskrud468@naver.com>
2025-05-22 23:10:55 +0900

    feat : 카카오 API에서 받아온 사용자 정보 DB에 저장

---
+++ main/src/main/java/com/sodam/KakaoRepository.java
@@ -0,0 +1,15 @@
+package com.sodam;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface KakaoRepository extends JpaRepository<KakaoUser, String> {
+
+    Optional<KakaoUser> findByid(Long id);
+    Optional<KakaoUser> findByNickname(String nickname);
+    Optional<KakaoUser> findByEmail(String email);
+
+}
