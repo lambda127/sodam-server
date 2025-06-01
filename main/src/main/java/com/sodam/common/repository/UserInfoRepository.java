17cca6f519f6cc3e227d64ec438e818bf2d4a0a1
nanakyeong <dlskrud468@naver.com>
2025-06-01 18:43:05 +0900

    revert: CORS 설정 및 머지 커밋 되돌리기

--- main/src/main/java/com/sodam/common/repository/UserInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sodam.common.repository;
-
-import com.sodam.common.entity.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
-}
